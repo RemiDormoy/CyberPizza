c6222f2ef8510e1a25a053c50cd5ca3d42afae88
Remi Dormoy <rdormoy@octo.com>
2020-06-27 12:20:20 +0200

    added colors

---
+++ lib/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color deepBlueDark = Color(0xFF03001F);
+const Color deepBlueMedium = Color(0xFF302670);
+const Color deepBlueLight = Color(0xFF676581);
+const Color white = Color(0xFFFFFFFF);
+const Color primary = Color(0xFFE601EC);
+const Color secondary = Color(0xFF0AFFFD);
+const Color accent = Color(0xFFF0EB09);
+const Color glow = Color(0xFF362FBC);
+const Color sunset = Color(0xFFF66721);
+
+const MaterialColor materialPrimary = MaterialColor(
+    0xFFE601EC,
+    const <int, Color> {
+      50: const Color(0xFFE601EC),
+      100: const Color(0xFFE601EC),
+      200: const Color(0xFFE601EC),
+      300: const Color(0xFFE601EC),
+      400: const Color(0xFFE601EC),
+      500: const Color(0xFFE601EC),
+      600: const Color(0xFFE601EC),
+      700: const Color(0xFFE601EC),
+      800: const Color(0xFFE601EC),
+      900: const Color(0xFFE601EC),
+    }
+);
