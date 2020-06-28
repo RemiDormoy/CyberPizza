04abe3ef4d1b1b4f2bf0967cdb1aaf5ffb68d418
Remi Dormoy <rdormoy@octo.com>
2020-06-28 16:27:57 +0200

    added glitch on pop up

--- lib/CustomDialog.dart
+++ lib/CustomDialog.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:cyberpizza/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomDialog extends StatelessWidget {
+class CustomDialog extends StatefulWidget {
   final String title, description, buttonText;
   final Image image;
 
@@ -14,6 +16,47 @@ class CustomDialog extends StatelessWidget {
     @required this.buttonText,
     this.image,
   });
+
+  @override
+  _CustomDialogState createState() => _CustomDialogState();
+}
+
+class _CustomDialogState extends State<CustomDialog> with TickerProviderStateMixin{
+
+  AnimationController glitchController;
+  Animation<double> glitchAnimation;
+
+  @override
+  void initState() {
+    glitchController =
+    AnimationController(duration: Duration(milliseconds: 10), vsync: this)
+      ..addListener(() => setState(() {}));
+    glitchAnimation = Tween(begin: 0.0, end: 1.0).animate(glitchController);
+    glitchController..addListener(() {
+      if (glitchController.status == AnimationStatus.completed) {
+        Timer(
+          Duration(milliseconds: 10),
+              () => {
+              glitchController.reverse()
+        },
+        );
+      } else if (glitchController.status == AnimationStatus.dismissed) {
+        Timer(
+          Duration(seconds: 2),
+              () => {
+            glitchController.forward()
+          },
+        );
+      }
+    });
+    Timer(
+      Duration(milliseconds: 300),
+          () => {
+        glitchController.forward()
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,94 +71,106 @@ class CustomDialog extends StatelessWidget {
   }
 
   dialogContent(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          width: double.infinity,
-          height: 160,
-          child: CustomPaint(
-              painter: DialogNeoPainter(),
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 15,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(30.0, 10, 10, 30),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: SvgPicture.asset(
-                              "assets/croix.svg",
-                              semanticsLabel: 'Acme Logo',
-                              color: Colors.black,
-                              width: 20,
-                              height: 20,
+    return Transform.translate(
+      offset: Offset(glitchAnimation.value * 5, glitchAnimation.value * 5),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 160,
+            child: CustomPaint(
+                painter: DialogNeoPainter(),
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 15,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(30.0, 10, 10, 30),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: SvgPicture.asset(
+                                "assets/croix.svg",
+                                semanticsLabel: 'Acme Logo',
+                                color: Colors.black,
+                                width: 20,
+                                height: 20,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '/Success;',
-                              style: TextStyle(
-                                color: primary,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w900,
-                                shadows: [
-                                  Shadow(
-                                    offset: Offset(-2, 0),
-                                    blurRadius: 2,
-                                    color: secondary,
-                                  ),
-                                ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '/Success;',
+                                style: TextStyle(
+                                  color: primary,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w900,
+                                  shadows: [
+                                    Shadow(
+                                      offset: Offset(-2, 0),
+                                      blurRadius: 2,
+                                      color: secondary,
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: Text(
-                                      'Cybernetic neck - \$234,99',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8.0),
+                                      child: Text(
+                                        'Cybernetic neck - \$234,99',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign: TextAlign.left,
                                       ),
-                                      textAlign: TextAlign.left,
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 6),
-                                    child: Text(
-                                      '/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: GoogleFonts.roboto().fontFamily
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 6),
+                                      child: Text(
+                                        '/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: GoogleFonts.roboto().fontFamily
+                                        ),
+                                        textAlign: TextAlign.left,
                                       ),
-                                      textAlign: TextAlign.left,
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 6.0),
-                                    child: Text(
-                                      'Your order will materialize soon.',
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 6.0),
+                                      child: Text(
+                                        'Your order will materialize soon.',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Thank you for shopping with us.',
                                       style: TextStyle(
                                         color: Colors.black,
                                         fontSize: 14,
@@ -123,29 +178,20 @@ class CustomDialog extends StatelessWidget {
                                       ),
                                       textAlign: TextAlign.left,
                                     ),
-                                  ),
-                                  Text(
-                                    'Thank you for shopping with us.',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              )),
-        ),
-        //...top circlular image part,
-      ],
+                )),
+          ),
+          //...top circlular image part,
+        ],
+      ),
     );
   }
 }
