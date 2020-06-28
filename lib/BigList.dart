65c46d8f986dc47c9a60ba893603aece8369aaf4
Remi Dormoy <rdormoy@octo.com>
2020-06-28 13:10:31 +0200

    added top sale

--- lib/BigList.dart
+++ lib/BigList.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 
+import 'package:cyberpizza/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class BigList extends StatefulWidget {
-
   Function(int) onPageChanged;
   int initialChild;
   _BigListState _state;
@@ -33,7 +34,8 @@ class _BigListState extends State<BigList> {
 
   @override
   void initState() {
-    pageController = PageController(initialPage: widget.initialChild, viewportFraction: viewPortFraction);
+    pageController = PageController(
+        initialPage: widget.initialChild, viewportFraction: viewPortFraction);
     pageController..addListener(_listener);
     super.initState();
   }
@@ -71,8 +73,10 @@ class _BigListState extends State<BigList> {
   }
 
   Widget _builder(BuildContext context, int position) {
-    final scale =
-        min(1.0, max(SCALE_FRACTION, (FULL_SCALE - (position - page).abs()) + viewPortFraction));
+    final scale = min(
+        1.0,
+        max(SCALE_FRACTION,
+            (FULL_SCALE - (position - page).abs()) + viewPortFraction));
     String asset;
     if (position == 0) {
       asset = "assets/eyeblue.png";
@@ -89,19 +93,62 @@ class _BigListState extends State<BigList> {
     } else if (position == 6) {
       asset = "assets/eyered.png";
     }
-    print("scale : " + scale.toString());
+    Widget topSale;
+    if (position % 5 == 1) {
+      topSale = Transform.translate(
+        offset: Offset(-40, 0),
+        child: Container(
+          child: CustomPaint(
+            painter: TopSalePainter(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/flamme.svg",
+                    semanticsLabel: 'Acme Logo',
+                    fit: BoxFit.contain,
+                    width: 15,
+                    height: 15
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, top: 2),
+                    child: Text("TOP SALE", style: TextStyle(fontSize: 12),),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    } else {
+      topSale = Container();
+    }
     return Transform.scale(
       scale: scale,
       child: GestureDetector(
         onTap: () {
-          if (position == pageController.page) Navigator.of(context).pushNamed("/detail");
+          if (position == pageController.page)
+            Navigator.of(context).pushNamed("/detail");
         },
         child: Container(
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
-            child: Image.asset(
-              asset,
-              fit: BoxFit.contain,
+            child: Stack(
+              children: [
+                Center(
+                  child: Image.asset(
+                    asset,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: topSale,
+                )
+              ],
             ),
           ),
         ),
@@ -110,7 +157,25 @@ class _BigListState extends State<BigList> {
   }
 
   setPage(int page) {
-    print('je jump');
     pageController.jumpToPage(page);
   }
+}
+
+class TopSalePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Color(0xFFD8D8D8).withOpacity(0.22)
+      ..style = PaintingStyle.fill;
+    var path = Path();
+    path..moveTo(20, 0);
+    path..lineTo(size.width, 0);
+    path..lineTo(size.width - 15, size.height);
+    path..lineTo(0, size.height);
+    path..lineTo(15, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
--- lib/ProductPage.dart
+++ lib/ProductPage.dart
@@ -24,26 +24,23 @@ class Product extends State<ProductPage> {
 
   @override
   Widget build(BuildContext context) {
-      var duration = 380;
-      if (_isAddedBeforeAnim == false) {
-        duration = 380;
-      } else {
-        duration = 94;
-      }
+    var duration = 380;
+    if (_isAddedBeforeAnim == false) {
+      duration = 380;
+    } else {
+      duration = 94;
+    }
     Timer(
         Duration(seconds: 0, milliseconds: duration),
-            () => {
+        () => {
               setState(() {
-                print("seting state");
                 if (_isAddedBeforeAnim == false) {
                   _isAddedBeforeAnim = true;
                 } else {
                   _isAnimated = true;
                 }
-
               })
-        });
-
+            });
 
     bigList = BigList(
       onPageChanged: _onProductFocusChanged,
@@ -51,117 +48,115 @@ class Product extends State<ProductPage> {
     );
     return Scaffold(
         body: Container(
-          decoration: BoxDecoration(color: deepBlueDark),
-          child: Stack(
+      decoration: BoxDecoration(color: deepBlueDark),
+      child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SvgPicture.asset(
+              "assets/grid.svg",
+              semanticsLabel: 'Acme Logo',
+              fit: BoxFit.fill,
+            ),
+          ),
+          animationAlphaChelou(),
+          Column(
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Container(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width,
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height,
-                child: SvgPicture.asset(
-                  "assets/grid.svg",
-                  semanticsLabel: 'Acme Logo',
-                  fit: BoxFit.fill,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(40.0, 50, 40, 20),
+                  child: Hero(
+                    tag: "titleAnimation",
+                    child: Opacity(
+                      opacity: 0.5,
+                      child: Image.asset(
+                        'assets/applogo.png',
+                        width: 200,
+                        height: 50,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              animationAlphaChelou(),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(40.0, 50, 40, 20),
-                      child: Hero(
-                        tag: "titleAnimation",
-                        child: Opacity(
-                          opacity: 0.5,
-                          child: Image.asset(
-                            'assets/applogo.png',
-                            width: 200,
-                            height: 50,
-                          ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    'Welcome back',
+                    style: TextStyle(
+                      color: white,
+                      fontSize: 32,
+                      shadows: [
+                        Shadow(
+                          offset: Offset.zero,
+                          blurRadius: 16.0,
+                          color: materialPrimary,
                         ),
-                      ),
+                        Shadow(
+                          offset: Offset.zero,
+                          blurRadius: 16.0,
+                          color: materialPrimary,
+                        ),
+                      ],
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: white,
-                          fontSize: 32,
-                          shadows: [
-                            Shadow(
-                              offset: Offset.zero,
-                              blurRadius: 16.0,
-                              color: materialPrimary,
-                            ),
-                            Shadow(
-                              offset: Offset.zero,
-                              blurRadius: 16.0,
-                              color: materialPrimary,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15),
-                      child: GestureDetector(
-                        onTap: () {
-                          ssshowDialog(context);
-                        },
-                        child: CyberButton(
-                          text: "Bionic arms",
-                          withArrow: true,
-                        ),
-                      ),
-                    ),
-                  ),
-                  // here
-    animationAlphaContent()
-                ],
+                ),
               ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: GestureDetector(
+                    onTap: () {
+                      ssshowDialog(context);
+                    },
+                    child: CyberButton(
+                      text: "Bionic arms",
+                      withArrow: true,
+                    ),
+                  ),
+                ),
+              ),
+              // here
+              animationAlphaContent()
             ],
           ),
-        ));
+        ],
+      ),
+    ));
   }
-
 
   animationAlphaChelou() {
     if (_isAddedBeforeAnim) {
     } else {
-      return  Container(
-      );
+      return Container();
     }
 
-    return AnimatedOpacity(
-        // If the widget is visible, animate to 0.0 (invisible).
-        // If the widget is hidden, animate to 1.0 (fully visible).
+//    return item;
+
+//    if (_isAnimated) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: AnimatedOpacity(
+          // If the widget is visible, animate to 0.0 (invisible).
+          // If the widget is hidden, animate to 1.0 (fully visible).
           opacity: _isAnimated ? 1.0 : 0.0,
           duration: Duration(milliseconds: 500),
           // The green box must be a child of the AnimatedOpacity widge
 
-          child: ChelouBackgroundViolet()
-      );
+          child: ChelouBackgroundViolet()),
+    );
+//    }
   }
-  animationAlphaContent() {
+
+  Widget animationAlphaContent() {
     if (_isAddedBeforeAnim) {
     } else {
-      return Container(
-
-      );
+      return Container();
     }
 
     return Expanded(
@@ -228,38 +223,45 @@ class Product extends State<ProductPage> {
 
   ssshowDialog(BuildContext context) {
     showDialog(
-    context: context,
-    builder: (BuildContext context) => CustomDialog(
-      title: "Success",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      buttonText: "Okay",
-    ),
+      context: context,
+      builder: (BuildContext context) => CustomDialog(
+        title: "Success",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        buttonText: "Okay",
+      ),
     );
   }
 
-  var images = ['assets/eyeblue.png',
-      'assets/neck.png',
-      'assets/helmet.png',
-      'assets/armleft.png',
-      'assets/pistolet.png',
-      'assets/armright.png',
-      'assets/eyered.png'];
+  var images = [
+    'assets/eyeblue.png',
+    'assets/neck.png',
+    'assets/helmet.png',
+    'assets/armleft.png',
+    'assets/pistolet.png',
+    'assets/armright.png',
+    'assets/eyered.png'
+  ];
 
-  var titles = ['/Blue zooming eye;',
+  var titles = [
+    '/Blue zooming eye;',
     '/Cybernetic neck;',
     '/Daft punk helmet;',
     '/Left power arm;',
     '/Energy boost (200ml);',
     '/Right extensible arm;',
-    '/Red laser eye;'];
+    '/Red laser eye;'
+  ];
 
-  var prices = ['€31,89',
+  var prices = [
+    '€31,89',
     '<New - €234,99',
     '€3231,89',
     '€610,56',
     '€11,00',
     '€543,90',
-    '€31,89'];
+    '€31,89'
+  ];
 
   _bottomList() {
     return ListView.builder(
@@ -304,7 +306,7 @@ class Product extends State<ProductPage> {
     }
     return GestureDetector(
       onTap: () {
-          bigList.setPage(position);
+        bigList.setPage(position);
       },
       child: Container(
         height: 40,
@@ -334,4 +336,3 @@ class Product extends State<ProductPage> {
     });
   }
 }
-
