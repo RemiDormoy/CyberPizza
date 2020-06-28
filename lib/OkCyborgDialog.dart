6fa2507a270aa332c3b41ce29c6715b10659e167
Remi Dormoy <rdormoy@octo.com>
2020-06-28 16:15:32 +0200

    yolo yolo yolo

--- lib/OkCyborgDialog.dart
+++ lib/OkCyborgDialog.dart
@@ -11,66 +11,114 @@ class OkCyborgDialog extends StatefulWidget {
 }
 
 class _OkCyborgDialogState extends State<OkCyborgDialog> {
-  var dialog = ["VOCAL IDENTIFICATION INITIATING..."];
+  var dialog = [""];
 
+  var wasPlayed0 = false;
   var wasPlayed1 = false;
   var wasPlayed2 = false;
   var wasPlayed3 = false;
   var wasPlayed4 = false;
 
+  buildFirstText() {
+    setState(() {
+      dialog.add("");
+    });
+    var sentence = "TELL ME YOUR DELIVERY ADDRESS";
+    for (var i = 0; i <= sentence.length; i++) {
+      Timer(
+        Duration(milliseconds: 50 * i),
+        () => {
+          setState(() {
+            dialog[1] = sentence.substring(0, i);
+          })
+        },
+      );
+    }
+  }
+
+  buildText2() {
+    setState(() {
+      dialog.add("");
+    });
+    var sentence = "14 AV CORBERA SET AS DELIVERY ADDRESS";
+    for (var i = 0; i <= sentence.length; i++) {
+      Timer(
+        Duration(milliseconds: 50 * i),
+        () => {
+          setState(() {
+            dialog[2] = sentence.substring(0, i);
+          })
+        },
+      );
+    }
+  }
+
+  buildText3() {
+    setState(() {
+      dialog.add("");
+    });
+    var sentence = "IDENTIFICATION SUCEEDED";
+    for (var i = 0; i <= sentence.length; i++) {
+      Timer(
+        Duration(milliseconds: 50 * i),
+        () => {
+          setState(() {
+            dialog[3] = sentence.substring(0, i);
+          })
+        },
+      );
+    }
+  }
+
+  buildText0() {
+    var sentence = "VOCAL IDENTIFICATION INITIATING...";
+    for (var i = 0; i <= sentence.length; i++) {
+      Timer(
+        Duration(milliseconds: 50 * i),
+        () => {
+          setState(() {
+            dialog[0] = sentence.substring(0, i);
+          })
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!wasPlayed0) {
+      //wasPlayed0 = true; Leave this bug because it creates a beautiful effect
+      buildText0();
+    }
     Timer(
         Duration(seconds: 2),
-            () =>
-        {
-          if (!wasPlayed1)
-            {
-              wasPlayed1 = true,
-              setState(() {
-                dialog.add("TELL ME YOUR DELIVERY ADDRESS");
-              })
-            }
-        });
+        () => {
+              if (!wasPlayed1) {wasPlayed1 = true, buildFirstText()}
+            });
     Timer(
         Duration(seconds: 4, milliseconds: 500),
-            () =>
-        {
-          if (!wasPlayed2)
-            {
-              wasPlayed2 = true,
-              setState(() {
-                dialog.add("14 AV CORBERA SET AS DELIVERY ADDRESS");
-              })
-            }
-        });
+        () => {
+              if (!wasPlayed2) {wasPlayed2 = true, buildText2()}
+            });
     Timer(
         Duration(seconds: 7),
-            () =>
-        {
-          if (!wasPlayed3)
-            {
-              wasPlayed3 = true,
-              setState(() {
-                dialog.add("IDENTIFICATION SUCEEDED");
-              })
-            }
-        });
+        () => {
+              if (!wasPlayed3) {wasPlayed3 = true, buildText3()}
+            });
     Timer(
         Duration(seconds: 9),
-            () =>
-        {
-          if (!wasPlayed4)
-            {
-              wasPlayed4 = true,
-              Navigator.of(context).pushNamed("/products", arguments: {"buyCompleted" : true})
+        () => {
+              if (!wasPlayed4)
+                {
+                  wasPlayed4 = true,
+                  Navigator.of(context)
+                      .pushNamed("/products", arguments: {"buyCompleted": true})
 
 //              Navigator.of(context).pushNamedAndRemoveUntil(
 //                  "/products", (route) => route.settings.name, )
 //              //hNamed(, arguments: {"buyCompleted" : true})
-
-            }
-        });
+                }
+            });
     return ListView.builder(
       itemCount: dialog.length,
       itemBuilder: _buildItem,
