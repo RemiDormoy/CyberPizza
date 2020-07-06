1276bed95990e3cf35226e95b6dbf474c4b265d7
Remi Dormoy <rdormoy@octo.com>
2020-07-06 13:13:17 +0200

    added some failing tests

---
+++ test/first_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Un test à la con qui doit passer', () {
+    expect(1, 1);
+  });
+
+  test('Un test à la con qui doit pas passer', () {
+    expect(1, 1);
+  });
+}
---
+++ test/second_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('yolo 1', () {
+    expect(1, 1);
+  });
+
+  test('yolo 2', () {
+    expect(1, 2);
+  });
+}
